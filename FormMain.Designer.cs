8c56b27026963e39213dfa00e9daa8831a94d7b3
cech12 <cech12@gmail.com>
2022-08-15 19:47:17 +0200

    fix blurry window  #40 - remove some unused libraries

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -521,8 +521,8 @@
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(874, 348);
             this.Controls.Add(this.descriptionLabel);
             this.Controls.Add(this.descriptionBox);
--- FormModSettings.Designer.cs
+++ FormModSettings.Designer.cs
@@ -92,8 +92,8 @@
             // 
             // FormModSettings
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(545, buttonY + 35);
             this.Controls.Add(this.closeButton);
             this.Controls.Add(this.setDefaultButton);
